123e43071ad229d4e68d31201f17e3b1597392e8
riquetta <37386067+riquetta@users.noreply.github.com>
2019-05-17 16:03:28 -0300

    Add files via upload

---
+++ cadastro.php
@@ -0,0 +1,33 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Cadastro realizado com sucesso!</title>
+</head>
+<body>
+<script>
+window.location.href = '/cadastro';
+</script>
+<?php require_once 'config.php'; ?>
+<?php
+$os = $_POST["os"];
+$cliente = $_POST["cliente"];	//atribuição do campo "email" vindo do formulário para variavel
+$tipo = $_POST["tipo"];	//atribuição do campo "ddd" vindo do formulário para variavel
+$operacao = $_POST["operacao"];
+$status = $_POST["status"];	//atribuição do campo "telefone" vindo do formulário para variavel
+$doca = $_POST["doca"];
+$dia = $_POST["dia"];
+$entrada = $_POST["entrada"];	//atribuição do campo "cidade" vindo do formulário para variavel
+$saida	= $_POST["saida"];	//atribuição do campo "estado" vindo do formulário para variavel
+$insere = mysql_query("INSERT INTO pcm (`cliente`,`os`, `tipo`, `operacao`, `status`, `doca`, `dia`, `entrada`, `saida`)
+VALUES('$cliente','$os','$tipo', '$operacao', '$status','$doca','$dia','$entrada','$saida')");
+if($insere)
+{
+        echo 'Dados Inseridos Com Sucesso.';
+}
+else{
+        echo 'Não Foi Possivel Inserir seus dados.';
+}
+
+?>
+</body>
+</html>
